6b209409bf3bc7a82734e39c42508153eeb66c07
Carl Fearby <carlfearby@me.com>
2024-09-28 03:13:02 +0100

    minor changes

--- Libs/ServiceCollectonExtensions.cs
+++ Libs/ServiceCollectonExtensions.cs
@@ -1,4 +1,6 @@
-namespace Webapi.Libs.User {
+using Webapi.Libs.User;
+
+namespace Webapi.Libs {
     public static class ServiceCollectionExtensions {
         public static void AddUserLogin(this IServiceCollection services, string jwtSecret) {
             services.AddScoped<UserLogin>(provider => new UserLogin(
--- Program.cs
+++ Program.cs
@@ -1,63 +1,43 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
-using Webapi.Libs.User;
 using Webapi.Libs;
-using Swashbuckle.AspNetCore.Annotations;
-using System.ComponentModel;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 
-// Add DbContext configuration
 builder.Services.AddDbContext<DatabaseContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
         new MySqlServerVersion(new Version(8, 0, 21))));
 
-// Add the connection string as a singleton service
 builder.Services.AddSingleton(builder.Configuration.GetConnectionString("DefaultConnection"));
-
-// Add Swagger services
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
-});
+builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" }); });
 
-// Add configuration for jwtSecret
 var jwtSecret = builder.Configuration.GetSection("JwtSettings:Secret").Value;
 
-// Register UserLogin with the jwtSecret
+if (string.IsNullOrEmpty(jwtSecret)) {
+    throw new ArgumentNullException(nameof(jwtSecret), "JWT Secret cannot be null or empty.");
+}
+
 builder.Services.AddUserLogin(jwtSecret);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+if (app.Environment.IsDevelopment()) {
     app.UseDeveloperExceptionPage();
-    // Enable middleware to serve generated Swagger as a JSON endpoint.
     app.UseSwagger();
-    // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
-    // specifying the Swagger JSON endpoint.
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
 }
-else
-{
+else {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.ConfigureEndpoints();
-
 app.Run();
